e011df1761de6d785d7f33dfd18415452159a48e
brightlitandaze4@gmail.com <brightlitandaze4@gmail.com>
2025-08-10 11:57:14 -0400

    Bright Update1

--- Program.cs
+++ Program.cs
@@ -1,14 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to C#, Jenkins Github lab");
-int num = 12;
-System.Console.WriteLine($"*Table of {num}*");
+﻿System.Console.WriteLine("Numbers 1 to 10");
 for (int i = 1; i <= 10; i++)
 {
-    System.Console.WriteLine($"{num}*{i}=\t {num * i}");
-}
-
-System.Console.WriteLine("Printing Numbers from 1 to 100");
-for (int i = 1; i <= 100; i++)
-{ 
     System.Console.Write(i+"\t");
 }
