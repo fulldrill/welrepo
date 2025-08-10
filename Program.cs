2a6ada9a1512a3a1552f91f4922cce7d9ef5e3b8
brightlitandaze4@gmail.com <brightlitandaze4@gmail.com>
2025-08-10 10:09:27 -0400

    second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to C#, Jenkins Github lab");
+int num = 12;
+System.Console.WriteLine($"*Table of {num}*");
+for (int i = 1; i <= 10; i++)
+{
+    System.Console.WriteLine($"{num}*{i}=\t {num * i}");
+}
+
+System.Console.WriteLine("Printing Numbers from 1 to 100");
+for (int i = 1; i <= 100; i++)
+{ 
+    System.Console.Write(i+"\t");
+}
--- obj/Debug/net9.0/DAY2.AssemblyInfo.cs
+++ obj/Debug/net9.0/DAY2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAY2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+661a2ec7f1417434de55f774672ef52ba8f54589")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAY2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAY2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
